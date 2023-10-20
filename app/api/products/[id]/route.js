74d7c841758a0fba64bb75d29692932508af3a7a
vishal <ivishalgautamm@gmail.com>
2023-10-20 12:45:46 +0530

    init

--- app/api/products/[id]/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import productsData from "@/store/productsData";
-
-export async function GET(request, { params: { id } }) {
-  const product = productsData.filter((product) => product.path === id);
-  return NextResponse.json(product);
-}
--- app/api/products/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import productsData from "@/store/productsData";
-
-export async function GET(request) {
-  return NextResponse.json(productsData);
-}
