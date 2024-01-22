7f413c63f1cf731f40899f4fe1e647812b464dcc
Vishal Gautam <ivishalgautamm@gmail.com>
2024-01-22 11:54:33 +0530

    added new metaTitle, metaDescription and metaKeywords in home, about, blogs, contact-us

--- app/about/layout.jsx
+++ app/about/layout.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   title: "About - PANAMAX LIMITED",
   description:
     "We are a leading manufacturer and supplier of adhesive tapes, specializing in customized industrial solutions. As a preferred OEM partner, we offer comprehensive services",
-  alternate: {
+  alternates: {
     canonical: "https://panamax.co.in/about",
   },
   openGraph: {
