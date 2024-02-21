9466ac90f32b9129697acef9abafa005f725508f
Vishal Gautam <ivishalgautamm@gmail.com>
2024-02-21 17:26:49 +0530

    added google tags

--- app/layout.js
+++ app/layout.js
@@ -82,6 +82,28 @@ export default function RootLayout({ children }) {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
       />
+      <script>
+        {(function (w, d, s, l, i) {
+          w[l] = w[l] || [];
+          w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+          var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != "dataLayer" ? "&l=" + l : "";
+          j.async = true;
+          j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, "script", "dataLayer", "GTM-NDFF5SCZ")}
+      </script>
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NDFF5SCZ"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+
       <body className={poppins.className}>
         <Providers>
           <Navbar />
